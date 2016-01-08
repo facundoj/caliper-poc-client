f41ed4170198d49ba633f21b44ceb10384cf67f2
facundoj <jaureguifacundo@gmail.com>
2016-01-08 15:13:20 -0300

    Start all

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ angular
         vm.addStudent = addStudent;
         vm.loadEvents = loadEvents;
         vm.start = start;
+        vm.startAll = startAll;
 
         function addStudent() {
             vm.students.push(StudentManager.create());
@@ -17,6 +18,12 @@ angular
 
         function loadEvents(student, filename) {
             StudentManager.loadEvents(student, filename);
+        }
+
+        function startAll() {
+            vm.students.forEach(function(student) {
+                StudentManager.startEvents(student);
+            });
         }
 
         function start(student) {
